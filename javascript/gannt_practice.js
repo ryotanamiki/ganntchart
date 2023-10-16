bdf3e19eb0891a7218b0321065136320f874027d
ryotanamiki <aaasti555@gmail.com>
2023-10-16 11:27:43 +0900

    日付選択

--- javascript/gannt_practice.js
+++ javascript/gannt_practice.js
@@ -18,7 +18,8 @@ $(function() {
 //課題01ーー複数行入れ替え
 	multiDrag($('#column_table_body'));
 
-//課題02--インデントright処理
+//課題02
+//インデントright処理
 	$('#indent_right').on('click', function () {
 		if ($selectRow != null) {
 			let indent = parseInt($selectRow.css('textIndent')) + indentW;
@@ -30,4 +31,13 @@ $(function() {
 		let indent = parseInt($selectRow.css('textIndent')) + indentW;
 		if (0 <= indent) $selectRow.css('textIndent', indent + 'px');
 	});
+
+//課題03
+	let tr = $('.column_header tr:nth-child(2)');
+	let days = $('.column_header tr:nth-child(2) th').length;
+
+	for (let i = 13; i <= days; i++) {
+		let date = $(tr.children('tr:nth-child(' + i + ')'));
+		dateState[tr.arr('id')] = date.position().left;
+	}
 });
