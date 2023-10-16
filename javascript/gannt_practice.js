e7d1fee023267090a314ccd918e10523b43dbc7c
ryotanamiki <aaasti555@gmail.com>
2023-10-16 10:41:46 +0900

    indent_righのクリックイベント

--- javascript/gannt_practice.js
+++ javascript/gannt_practice.js
@@ -1,11 +1,28 @@
+let $selectRow = null;
+let selectingRow = 'ui-selected';
+const indentW = 12;
+
+
 // 初期表示時
+//readyベント
 $(function() {
-	
+
 	// CSS関連 - ヘッダー
 	const element = document.querySelector('.column_header');
 	// 横にスクロールした分、位置をずらす
 	element.style.left = -window.pageXOffset + 'px';
 	window.addEventListener('scroll', () => {
-	  element.style.left = -window.pageXOffset + 'px';
-	});
+	element.style.left = -window.pageXOffset + 'px';
+});
+
+//課題01ーー複数行入れ替え
+	multiDrag($('#column_table_body'));
+
+//課題02--インデント処理
+	$('#indent_right').on('click', function () {
+		if ($selectRow != null) {
+			let indent = parseInt($selectRow.css('textIndent')) + indentW
+			if (indent <= (indentW * 3)) $selectRow.css('textIndent', indent + 'px');
+		}
+	})
 });
