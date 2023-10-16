36a6e1eb1b8e8a03b650406a71a65a96254c1dac
ryotanamiki <aaasti555@gmail.com>
2023-10-16 17:50:24 +0900

    複数選択機能修正

--- javascript/gannt_practice.js
+++ javascript/gannt_practice.js
@@ -1,9 +1,8 @@
-let $selectRow = null;
-let selectingRow = 'ui-selected';
-const indentW = 12;
-let dateState = [];
-const widthAdd = 19;
-
+let $selectedRow = null;
+let selectRow = 'ui-selected';
+let indentWtd = 12;
+let dataArea = [];
+let widAdd = 19;
 
 // 初期表示時
 //readyベント
@@ -18,66 +17,67 @@ $(function() {
 });
 
 //課題01ーー複数行入れ替え
-	multiDrag($('#column_table_body'));
+	multiDrag($("#column_table_body"));
 
 //課題02
-//インデントright処理
-	$('#indent_right').on('click', function () {
-		if ($selectRow != null) {
-			let indent = parseInt($selectRow.css('textIndent')) + indentW;
-			if (indent <= (indentW * 3)) $selectRow.css('textIndent', indent + 'px');
+//インデントright
+	$("#indent_right").on('click', function () {
+		if ($selectedRow != null) {
+			let indent = parseInt($selectedRow.css("textIndent")) + indentWtd;
+			if (indent <= (indentWtd * 3)) $selectedRow.css("textIndent", indent + "px");
 		}
 	});
-//インデントleft処理
-	$('#indent_left').on('click', function () {
-		if($selectedRow != null){
-		let indent = parseInt($selectRow.css('textIndent')) + indentW;
-		if (0 <= indent) $selectRow.css('textIndent', indent + 'px');
-	}
+//インデントleft
+	$("#indent_left").on('click', function () {
+		if ($selectedRow != null) {
+			let indent = parseInt($selectedRow.css("textIndent")) + indentWtd;
+			if (0 <= indent) $selectedRow.css("textIndent", indent + "px");
+		}
 	});
 
-//課題03
-	let tr = $('.column_header tr:nth-child(2)');
-	let days = $('.column_header tr:nth-child(2) th').length;
+//日付
+	let $tarTr = $(".column_header tr:nth-child(2)");
+	let dayLen = $(".column_header tr:nth-child(2) th").length;
 
-	for (let i = 13; i <= days; i++) {
-		let date = $(tr.children('tr:nth-child(' + i + ')'));
-		dateState[date.attr('id')] = date.position().left;
+	for(let i = 13; i <= dayLen; i++){
+		let $dateTh = $($tarTr.children("th:nth-child(" + i + ")"));
+		dataArea[$dateTh.attr("id")] = $dateTh.position().left;
 	}
 });
 
-//複数選択
-function multiDrag(element) {
-	$(element).selectable({
-		cancel: 'sort-handle, .ui-selected',
+//複数選択ドラッグ
+function multiDrag(emt) {
+	$(emt).selectable({
+		cancel: '.sort-handle, .ui-selected',
 
-		selected: function (a, b) {
-			$selectRow = $('.' + selectingRow).children('td:nth-child(2)');
+		selected: function (e, ui) {
+			$selectedRow = $("." + selectRow).children("td:nth-child(2)");
 		}
 	}).sortable({
 		axis: 'y',
-		items: '> tr',
-		handle: 'td, .sort-handle. .ui-selected',
-		helper: function (a, c) {
-			if(!c.hasClass('ui-selected')) {
-			c.parent().children('.ui-selected').removeClass('ui-selected');
-			c.addClass('ui-selected');
+		opacity: 0.9,
+		item: "> tr",
+		handle: 'td, .sort-handle, .ui-selected',
+		helper: function (e, item) {
+			if (!item.hasClass('ui-selected')) {
+				item.parent().children('.ui-selected').removeClass('ui-selected');
+				item.addClass('ui-selected');
 			}
-			let selected = c.parent().children('.ui-selected').clone();
-			high = c.Height() * selected.length;
-			c.data('multidrag', selected).sibling('.ui-selected').remove();
-			return$('<tr/>').append(selected);
+			let selected = item.parent().children('.ui-selected').clone();
+			ph = item.outerHeight() * selected.length;
+			item.data('multidrag', selected).siblings('ui-selected').remove();
+			return $('<tr/>').append(selected);
 		},
-		cursor: 'move',
-		start: function (a, d) {
-			d.placeholder.css({'height':high});
+		cursor: "move",
+		start: function (e, ui) {
+			ui.placeholder.css({ 'height': ph });
 		},
-		stop: function (a, d) {
-			let selected = d.item.data('multidrag');
-			d.item.after(selected);
-			d.item.remove();
-			selected.removeClass('ui-selected');
-			$(selected).children('td').removeClass('ui-selected');
+		stop: function (e, ui) {
+			let selected = ui.item.data('multidrag');
+			ui.item.after(selected);
+			ui.item.remove();
+			selected.removeClass();
+			$(selected).children("td").removeClass('ui-selected');
 		}
-	})
+	});
 }
